cecafdce3bf1c6743649d365bbe5401c2001b0f8
Carlos <carlos.ruiz@ellucian.com>
2023-03-30 11:03:02 -0600

    Add css globals and delete boilerplate code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
