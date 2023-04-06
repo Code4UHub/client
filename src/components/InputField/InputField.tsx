52d464c79602c749a3a81a33d3b652a144a24d42
Carlos Vega <A01731416@itesm.mx>
2023-04-06 00:55:56 -0600

    [INPUT FIELD] added field to feedback on correct input

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { correctState } from 'components/Form/inputRules';
 import style from './InputField.module.css';
 
 type Props = {
@@ -8,7 +9,16 @@ type Props = {
   className: string;
   required: boolean;
   error: string;
+  value: string;
+  handleBlur: (id: string, value: string) => void;
+  handleChange: (id: string, value: string) => void;
 };
+
+function decideSpanClass(error: string) {
+  if (error === correctState) return style.correct;
+  if (error !== "") return style.error;
+  return style.hidden;
+}
 
 export default function InputField({
   label,
@@ -17,16 +27,21 @@ export default function InputField({
   className,
   required,
   error,
+  value,
+  handleChange,
+  handleBlur,
 }: Props) {
   return (
     <div className={`${className} ${style.inputField}`}>
       <label htmlFor={id}>{label}</label>
-      <span className={style.error}>{error}</span>
+      <span className={decideSpanClass(error)}>{error}</span>
       <input
         id={id}
         type={type}
         className={style.inputHero}
         required={required}
+        onBlur={() => handleBlur(id, value)}
+        onChange={(e) => handleChange(id, e.target.value)}
       />
     </div>
   );
