aee5beff2b32607544b0eb306ca01cc2dd15fb13
Carlos Vega <A01731416@itesm.mx>
2023-04-06 00:56:41 -0600

    [FORM] Added onChange and onBlur handler

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,61 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import InputField from 'components/InputField/InputField';
 import Button from 'components/Button/Button';
+import { inputData } from './inputData';
+import { inputRules } from './inputRules';
 import style from './Form.module.css';
 
-type InputData = {
-  label: string;
-  type: string;
-  id: string;
-}[];
-
-const inputData: { [key: string]: InputData } = {
-  signIn: [
-    {
-      label: 'Correo',
-      type: 'email',
-      id: 'email',
-    },
-    {
-      label: 'Contraseña',
-      type: 'password',
-      id: 'password',
-    },
-  ],
-  signUp: [
-    {
-      label: 'Nombre(s)',
-      type: 'text',
-      id: 'firstName',
-    },
-    {
-      label: 'Apellido(s)',
-      type: 'text',
-      id: 'lastName',
-    },
-    {
-      label: 'Correo',
-      type: 'email',
-      id: 'email',
-    },
-    {
-      label: 'Contraseña',
-      type: 'password',
-      id: 'password',
-    },
-    {
-      label: 'Confirmar contraseña',
-      type: 'password',
-      id: 'passwordConfirmation',
-    },
-  ],
-};
 
 type Props = {
   screen: 'signIn' | 'signUp';
 };
 
-export default function FormHero({ screen }: Props) {
+export default function Form({ screen }: Props) {
+  const [inputErrors, setInputErrors] = useState<{ [key: string]: string }>({
+  });
+  const [inputValues, setInputValues] = useState<{ [key: string]: string }>({
+  });
+  useEffect(() => {
+    setInputErrors({});
+    inputData[screen].forEach(({id}) => {
+      setInputErrors((inputE) => ({...inputE, [id]: ""}))
+    });
+    setInputValues({});
+    inputData[screen].forEach(({id}) => {
+      setInputValues((value) => ({...value, [id]: ""}))
+    });
+  }, [screen]);
+  
+  function onChangeHandler(id : string, value : string) {
+    setInputValues((storedValues) => ({...storedValues, [id]: value}))
+  }
+
+  function onCheckRules(id : string, value : string) {
+    const rule = inputRules.find((r) => r.id === id);
+    if (rule) {
+      const validationResult = rule.validate(value);
+      setInputErrors((prevErrors) => ({ ...prevErrors, [id]: validationResult }));
+    }
+  }
+
   const onMainClick = () => console.log('Holi');
 
   return (
@@ -70,11 +52,14 @@ export default function FormHero({ screen }: Props) {
           {inputData[screen].map((field, index: number) => (
             <InputField
               key={field.id}
+              value={inputValues[field.id]}
               label={field.label}
               type={field.type}
               id={field.id}
-              error="Utiliza tu correo institucional"
+              error={inputErrors[field.id]}
               required
+              handleChange={onChangeHandler}
+              handleBlur={onCheckRules}
               className={
                 screen === 'signUp' && index < 2
                   ? style.halfInput
