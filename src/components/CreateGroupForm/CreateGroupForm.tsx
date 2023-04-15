0e4002292d14a6d9acc37adf935eaff5b681c42b
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-04-15 17:43:11 -0600

    [CREATE GROUP] Finish errors implementation

--- src/components/CreateGroupForm/CreateGroupForm.tsx
+++ src/components/CreateGroupForm/CreateGroupForm.tsx
@@ -1,38 +1,39 @@
-import React, { useState, useRef, useEffect, useReducer } from "react";
-import { InputField } from "components/InputField/InputField";
-import { Button } from "components/Button/Button";
-// eslint-disable-next-line
-import { createGroupInputData, days } from "./createGroupData";
-import { inputRules } from "./inputRules";
-import styles from "./CreateGroupFom.module.css";
-import { ReactComponent as IconClose } from "./x-mark.svg";
+import React, { useState, useRef, useEffect, useReducer } from 'react';
+import { InputField } from 'components/InputField/InputField';
+import { Button } from 'components/Button/Button';
+import { createGroupInputData, days } from './createGroupData';
+import { inputRules } from './inputRules';
+import styles from './CreateGroupFom.module.css';
+import { ReactComponent as IconClose } from './x-mark.svg';
 
 const query = [
-  { id: 1, name: "Mathematics" },
-  { id: 2, name: "Physics" },
-  { id: 3, name: "Chemistry" },
-  { id: 4, name: "Biology" },
-  { id: 5, name: "Computer Science" },
-  { id: 6, name: "English" },
-  { id: 7, name: "History" },
-  { id: 8, name: "Geography" },
-  { id: 9, name: "Art" },
-  { id: 10, name: "Music" },
-  { id: 11, name: "Physical Education" },
-  { id: 12, name: "Social Studies" },
-  { id: 13, name: "Environmental Science" },
-  { id: 14, name: "Foreign Language" },
-  { id: 15, name: "Health Education" },
-  { id: 16, name: "Economics" },
-  { id: 17, name: "Psychology" },
-  { id: 18, name: "Sociology" },
-  { id: 19, name: "Political Science" },
-  { id: 20, name: "Philosophy" },
+  { id: 1, name: 'Mathematics' },
+  { id: 2, name: 'Physics' },
+  { id: 3, name: 'Chemistry' },
+  { id: 4, name: 'Biology' },
+  { id: 5, name: 'Computer Science' },
+  { id: 6, name: 'English' },
+  { id: 7, name: 'History' },
+  { id: 8, name: 'Geography' },
+  { id: 9, name: 'Art' },
+  { id: 10, name: 'Music' },
+  { id: 11, name: 'Physical Education' },
+  { id: 12, name: 'Social Studies' },
+  { id: 13, name: 'Environmental Science' },
+  { id: 14, name: 'Foreign Language' },
+  { id: 15, name: 'Health Education' },
+  { id: 16, name: 'Economics' },
+  { id: 17, name: 'Psychology' },
+  { id: 18, name: 'Sociology' },
+  { id: 19, name: 'Political Science' },
+  { id: 20, name: 'Philosophy' },
 ];
 
 const INPUT_ERRORES_INITIAL = createGroupInputData.reduce(
   (acc: { [key: string]: string }, { id }) => {
-    acc[id] = "";
+    if (id !== 'days') {
+      acc[id] = '';
+    }
     return acc;
   },
   {}
@@ -44,22 +45,24 @@ const inputErrorsReducer = (
 ): typeof INPUT_ERRORES_INITIAL => {
   const { type, payload } = action;
   switch (type) {
-    case "UPDATE_ERRORS":
+    case 'UPDATE_ERRORS':
       return { ...state, ...payload };
 
     default:
-      throw Error("Type not defined");
+      throw Error('Type not defined');
   }
 };
 
 const INPUT_VALUES_INITIAL = createGroupInputData.reduce(
-  (acc: { [key: string]: string }, { id }) => {
-    if (id === "days") {
+  (acc: { [key: string]: string | { id: number; name: string } }, { id }) => {
+    if (id === 'subject') {
+      acc[id] = { id: NaN, name: '' };
+    } else if (id === 'days') {
       days.forEach((day) => {
-        acc[day] = "off";
+        acc[day] = 'off';
       });
     } else {
-      acc[id] = "";
+      acc[id] = '';
     }
     return acc;
   },
@@ -73,11 +76,11 @@ const inputValuesReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case "UPDATE_VALUES":
+    case 'UPDATE_VALUES':
       return { ...state, ...payload };
 
     default:
-      throw Error("Type not defined");
+      throw Error('Type not defined');
   }
 };
 
@@ -88,6 +91,8 @@ export default function CreateGroupForm() {
 
   const [isListOpen, setIsListOpen] = useState(false);
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+
+  let timeOutId: NodeJS.Timeout;
 
   // eslint-disable-next-line
   const [inputValues, dispatch] = useReducer(
@@ -109,55 +114,134 @@ export default function CreateGroupForm() {
     setFilteredClasses(query);
   }, []);
 
+  const checkFormValidation = () => {
+    const errors = Object.values(inputErrors);
+
+    for (let i = 0; i < errors.length; i += 1) {
+      if (errors[i] !== 'Hecho') {
+        setIsSubmitDisabled(true);
+        return;
+      }
+    }
+
+    if (
+      inputValues.L === 'off' &&
+      inputValues.M === 'off' &&
+      inputValues.X === 'off' &&
+      inputValues.J === 'off' &&
+      inputValues.V === 'off'
+    ) {
+      setIsSubmitDisabled(true);
+      return;
+    }
+
+    setIsSubmitDisabled(false);
+  };
+
+  useEffect(() => {
+    checkFormValidation();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    inputErrors,
+    inputValues.L,
+    inputValues.M,
+    inputValues.X,
+    inputValues.J,
+    inputValues.V,
+  ]);
+
   // filters autocomplete results based on user inputz
   const filterClass = (value: string) => {
-    const newClass = classes.filter(({ name }) => name.includes(value));
+    const newClass = classes.filter(
+      ({ id, name }) =>
+        `${id} - ${name}`.includes(value.trim()) ||
+        name.includes(value.trim()) ||
+        `${id}`.includes(value.trim())
+    );
 
     setFilteredClasses(newClass);
   };
 
   // Updates input values every time a field changes
   const onChangeHandler = (id: string, value: string) => {
-    if (id === "subject") {
+    setIsSubmitDisabled(true);
+    if (inputErrors[id])
+      dispatchError({ type: 'UPDATE_ERRORS', payload: { [id]: '' } });
+
+    if (id === 'subject') {
       filterClass(value);
     }
     if (days.includes(id)) {
-      const newState = inputValues[id] === "off" ? "on" : "off";
-      dispatch({ type: "UPDATE_VALUES", payload: { [id]: newState } });
+      const newState = inputValues[id] === 'off' ? 'on' : 'off';
+      dispatch({ type: 'UPDATE_VALUES', payload: { [id]: newState } });
     } else {
-      dispatch({ type: "UPDATE_VALUES", payload: { [id]: value } });
+      dispatch({ type: 'UPDATE_VALUES', payload: { [id]: value } });
     }
   };
 
-  const onCheckRules = (id: string, value: string | undefined) => {
+  const onCheckRules = (
+    id: string,
+    value: string | { id: number; name: string }
+  ) => {
     const rule = inputRules.find((r) => r.id === id);
-    let validationResult = "";
-    if (rule && value) {
-      validationResult = rule.validate(value);
-      console.log(validationResult);
+    let validationResult = '';
+    if (rule) {
+      if (id === 'endTime')
+        validationResult = rule.validate(value, inputValues.startTime);
+      else validationResult = rule.validate(value);
     }
+
+    dispatchError({
+      type: 'UPDATE_ERRORS',
+      payload: { [id]: validationResult },
+    });
+
+    if (
+      id === 'startTime' &&
+      validationResult === 'Hecho' &&
+      inputErrors.endTime
+    )
+      onCheckRules('endTime', inputValues.endTime);
   };
 
   // Updates class on item click
-  const selectClass = (id: string, name: string) => {
-    if (autoComplete.current) autoComplete.current.value = name;
+  const selectClass = (id: number, name: string) => {
+    if (autoComplete.current) autoComplete.current.value = `${id} - ${name}`;
 
     dispatch({
-      type: "UPDATE_VALUES",
-      payload: { subject: name },
+      type: 'UPDATE_VALUES',
+      payload: { subject: { id, name } },
     });
+
+    if (inputErrors.subject) {
+      dispatchError({
+        type: 'UPDATE_ERRORS',
+        payload: { subject: '' },
+      });
+    }
 
     setIsListOpen(false);
   };
 
+  const onBlurHandler = () => {
+    timeOutId = setTimeout(() => {
+      setIsListOpen(false);
+      onCheckRules('subject', inputValues.subject);
+    });
+  };
+
+  const onFocusHandler = () => {
+    clearTimeout(timeOutId);
+  };
+
   // Closes autocomplete list on tab press
   const skipAutocomplete = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Tab") setIsListOpen(false);
+    if (e.key === 'Tab') setIsListOpen(false);
   };
 
   // Traps tab focus on modal
   const trapFocus = (e: React.KeyboardEvent<HTMLButtonElement>) => {
-    if (e.key === "Tab") {
+    if (e.key === 'Tab') {
       if (e.shiftKey) {
         /* shift + tab */ if (
           document.activeElement === firstFocusableEl.current
@@ -175,12 +259,14 @@ export default function CreateGroupForm() {
   // Renders inputfield based on inputData
   const inputFields = createGroupInputData.map((inputData) => {
     switch (inputData.id) {
-      case "subject":
+      case 'subject':
         return (
           <div
             key={inputData.id}
             className={`${styles.autocomplete}`}
-            onBlur={() => onCheckRules("subject", autoComplete.current?.value)}
+            tabIndex={-1}
+            onBlur={onBlurHandler}
+            onFocus={onFocusHandler}
           >
             <InputField
               placeholder={inputData.placeholder}
@@ -198,28 +284,29 @@ export default function CreateGroupForm() {
               handleBlur={() => {}}
             />
             {isListOpen && (
-              <ul className={styles["autocomplete-list"]}>
+              <ul className={styles['autocomplete-list']}>
                 {filteredClasses.map(({ id, name }) => (
-                  <li key={id} onClick={() => selectClass(inputData.id, name)}>
-                    {name}
+                  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+                  <li key={id} onClick={() => selectClass(id, name)}>
+                    {`${id} - ${name}`}
                   </li>
                 ))}
               </ul>
             )}
           </div>
         );
-      case "days":
+      case 'days':
         return (
           <div key={inputData.id} className={styles.singleInput}>
             <legend>{inputData.label}</legend>
-            <div className={styles["day-buttons"]}>
+            <div className={styles['day-buttons']}>
               {days.map((day) => (
                 <Button
-                  className={inputValues[day] === "on" ? styles.selected : ""}
+                  className={inputValues[day] === 'on' ? styles.selected : ''}
                   key={day}
                   location="createGroup"
                   text={day}
-                  onClickHandler={() => onChangeHandler(day, "off")}
+                  onClickHandler={() => onChangeHandler(day, 'off')}
                   isDisable={false}
                 />
               ))}
@@ -231,11 +318,11 @@ export default function CreateGroupForm() {
           <InputField
             key={inputData.id}
             className={`${styles.button} ${
-              inputData.id === "startTime" || inputData.id === "endTime"
+              inputData.id === 'startTime' || inputData.id === 'endTime'
                 ? styles.halfInput
                 : styles.singleInput
             }`}
-            value=""
+            value={inputValues[inputData.id] as string}
             placeholder={inputData.placeholder}
             label={inputData.label}
             type={inputData.type}
@@ -250,11 +337,11 @@ export default function CreateGroupForm() {
   });
 
   return (
-    <div className={styles["form-wrapper"]}>
-      <div className={styles["form-container"]}>
-        <div className={styles["close-button-container"]}>
+    <div className={styles['form-wrapper']}>
+      <div className={styles['form-container']}>
+        <div className={styles['close-button-container']}>
           <button
-            className={styles["close-button"]}
+            className={styles['close-button']}
             type="button"
             ref={firstFocusableEl}
             onKeyDown={trapFocus}
@@ -264,7 +351,7 @@ export default function CreateGroupForm() {
         </div>
         <form autoComplete="off">
           <h3>Crear Grupo</h3>
-          <div className={styles["form-inputs"]}>{inputFields}</div>
+          <div className={styles['form-inputs']}>{inputFields}</div>
           <Button
             location=""
             text="Crear grupo"
