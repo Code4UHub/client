86364cee32b47aa7a0bcef9ae5ce28a5ec0c9cd1
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-04-15 21:43:36 -0600

    [LOGIN SIGNUP] Create user reducer and protect paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,37 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Authentication from 'routes/authentication/Authentication';
-import App from "./App";
-import "./index.css";
+import { store } from 'store/store';
+import { Provider } from 'react-redux';
+import { Root } from 'routes/root/Root';
+import App from './App';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
-    errorElement: <h1>Error</h1>
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+    ],
+    errorElement: <h1>Error</h1>,
   },
   {
-    path: 'auth',
-    element: <Authentication />
-  }
-])
+    path: '/auth',
+    element: <Authentication />,
+  },
+]);
 
-const container = document.getElementById("root")!;
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
