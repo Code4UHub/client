9882565204d734432131c1ae22e0bc3d567c6464
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-04-23 15:46:10 -0600

    [JOIN GROUP] Finish UI logic

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,39 +1,35 @@
-import React from "react";
-import style from "./Button.module.css";
+import React from 'react';
+import style from './Button.module.css';
 
 type Props = {
   className?: string;
   location: string;
   text: string;
   onClickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  type?: string;
+  type?: 'button' | 'submit' | 'reset';
   isDisable?: boolean;
   onKeyDownHandler?: (e: React.KeyboardEvent<HTMLButtonElement>) => void;
 };
 
 export const Button = React.forwardRef<HTMLButtonElement, Props>(
-  (props, ref) => {
-    const isSubmit: boolean = props.type === "submit";
-
-    return (
-      <button
-        className={`${props.className} ${style.button} ${style[props.location]
-          }`}
-        onClick={props.onClickHandler}
-        disabled={props.isDisable}
-        type={isSubmit ? "submit" : "button"}
-        ref={ref}
-        onKeyDown={props.onKeyDownHandler}
-      >
-        {props.text}
-      </button>
-    );
-  }
+  (props, ref) => (
+    <button
+      className={`${props.className} ${style.button} ${style[props.location]}`}
+      onClick={props.onClickHandler}
+      disabled={props.isDisable}
+      // eslint-disable-next-line react/button-has-type
+      type={props.type}
+      ref={ref}
+      onKeyDown={props.onKeyDownHandler}
+    >
+      {props.text}
+    </button>
+  )
 );
 
 Button.defaultProps = {
-  className: "",
-  type: "button",
+  className: '',
+  type: 'button',
   isDisable: false,
-  onKeyDownHandler: () => { },
+  onKeyDownHandler: () => {},
 };
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,85 +5,111 @@ import styles from './Modal.module.css';
 import { ReactComponent as IconClose } from './x-mark.svg';
 
 type ModalProps = {
-    title: string,
-    open: Boolean,
-    onClose: Function,
-    lastFocusableElement: React.RefObject<HTMLElement>,
-    children: React.ReactNode;
-}
+  title: string;
+  open: Boolean;
+  onClose: Function;
+  lastFocusableElement: React.RefObject<HTMLElement>;
+  children: React.ReactNode;
+  current?: string;
+};
 
-export default function Modal({ title, open, onClose, lastFocusableElement, children }: ModalProps) {
+export default function Modal({
+  title,
+  open,
+  onClose,
+  lastFocusableElement,
+  children,
+  current,
+}: ModalProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const closeRef = useRef<HTMLButtonElement>(null);
+  const topRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
 
-    const closeRef = useRef<HTMLButtonElement>(null);
-    const topRef = useRef<HTMLDivElement>(null);
-    const bottomRef = useRef<HTMLDivElement>(null);
+  const closeModal = () => {
+    setIsModalOpen(false);
+    onClose();
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-        onClose();
+  useEffect(() => {
+    const lastFocusableEl = lastFocusableElement;
+
+    const trapFocus = (e: FocusEvent) => {
+      if (e.target === topRef.current) {
+        lastFocusableEl.current?.focus();
+      } else if (e.target === bottomRef.current) {
+        closeRef.current?.focus();
+      }
     };
 
-    useEffect(() => {
+    const escapeClose = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setIsModalOpen(false);
+        onClose();
+      }
+    };
 
-        const lastFocusableEl = lastFocusableElement;
+    document.addEventListener('focusin', trapFocus);
+    document.addEventListener('keydown', escapeClose);
 
-        const trapFocus = (e: FocusEvent) => {
-            if (e.target === topRef.current) {
-                lastFocusableEl.current?.focus();
-            } else if (e.target === bottomRef.current) {
-                closeRef.current?.focus();
-            }
-        };
+    return function removeListener() {
+      document.removeEventListener('focusin', trapFocus);
+      document.removeEventListener('keydown', escapeClose);
+    };
 
-        const escapeClose = (e: KeyboardEvent) => {
-            if (e.key === 'Escape') {
-                setIsModalOpen(false);
-                onClose();
-            }
-        };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastFocusableElement, isModalOpen]);
 
-        document.addEventListener('focusin', trapFocus);
-        document.addEventListener('keydown', escapeClose);
+  useEffect(() => {
+    if (open) {
+      closeModal();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [open]);
 
-        return function removeListener() {
-            document.removeEventListener('focusin', trapFocus);
-            document.removeEventListener('keydown', escapeClose);
-        }
+  useEffect(() => {
+    const modalContainer = modalRef.current;
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [lastFocusableElement, isModalOpen]);
+    if (isModalOpen) {
+      modalContainer?.classList.add(styles.fadein);
+      setTimeout(() => {
+        modalContainer?.classList.remove(styles.fadein);
+      }, 400);
+    }
+  }, [isModalOpen, current]);
 
-    useEffect(() => {
-        if (open) {
-            closeModal();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [open]);
-
-
-    return (
-        <>
-            <Button text={title} onClickHandler={() => setIsModalOpen(true)} location='' />
-            {isModalOpen &&
-                <div className={styles['modal-wrapper']}>
-                    <div tabIndex={0} ref={topRef} />
-                    <div className={styles['modal-container']}>
-                        <div className={styles['close-button-container']}>
-                            <button
-                                className={styles['close-button']}
-                                type="button"
-                                ref={closeRef}
-                                onClick={closeModal}
-                            >
-                                <IconClose />
-                            </button>
-                        </div>
-                        {children}
-                    </div>
-                    <div tabIndex={0} ref={bottomRef} />
-                </div>}
-        </>
-    );
+  return (
+    <>
+      <Button
+        text={title}
+        onClickHandler={() => setIsModalOpen(true)}
+        location=""
+      />
+      {isModalOpen && (
+        <div className={styles['modal-wrapper']}>
+          <div tabIndex={0} ref={topRef} />
+          <div className={styles['modal-container']} ref={modalRef}>
+            <div className={styles['close-button-container']}>
+              <button
+                className={styles['close-button']}
+                type="button"
+                ref={closeRef}
+                onClick={closeModal}
+              >
+                <IconClose />
+              </button>
+            </div>
+            <div className={styles['modal-content']}>{children}</div>
+          </div>
+          <div tabIndex={0} ref={bottomRef} />
+        </div>
+      )}
+    </>
+  );
 }
+
+Modal.defaultProps = {
+  current: '',
+};
