d0f3d8e36a80e00990a91ddaa778e81d9c907249
Carlos Vega <60989884+CarLosVegga@users.noreply.github.com>
2023-04-24 17:10:09 -0600

    Revert "Signin signup components"

--- src/utils/errorMessage/generalErrorMessage.ts
+++
@@ -1,7 +0,0 @@
-
-export enum GeneralError {
-    noSpecialChar = "Entrada no válida",
-    noEmpty = "Campo obligatorio. No lo deje vacío",
-    maxCharError = "Excede el máximo de ",
-    minCharError = "Incluya mínimo "
-}
