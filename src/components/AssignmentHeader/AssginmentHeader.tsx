fe287626805acf67db5ceeb7591e975eb53512ed
Carlos Vega <A01731416@itesm.mx>
2023-04-26 00:32:43 -0600

    [ASSIGNMENT] Moved Title to AssigmentHeader and included Send Assignment button

---
+++ src/components/AssignmentHeader/AssginmentHeader.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button } from 'components/Button/Button';
+import styles from './AssignmentHeader.module.css';
+
+type Props = {
+  title: string;
+  onClickButton: () => void;
+  isButtonDisabled: boolean;
+};
+
+export default function AssignmentHeader({ title, onClickButton, isButtonDisabled }: Props) {
+  return (
+    <div className={styles['assignment-header']}>
+      <div className={styles['assignment-header-container']}>
+        <h1>{title}</h1>
+        <Button
+            location="assignmentSubmit"
+            text="Terminar examen"
+            onClickHandler={onClickButton}
+            type="submit"
+            isDisable={isButtonDisabled}
+          />
+      </div>
+      <hr />
+    </div>
+  );
+}
