82bc7962e13096fa55941fceb0da1232b787f0e1
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-05-06 10:33:17 -0600

    [TYPES] Refactored types to use generic promise type

---
+++ src/types/Class/Class.ts
@@ -0,0 +1,15 @@
+import { TypePromise } from 'types/TypePromise/TypePromise';
+
+export type Class = {
+  class_id: string;
+  finished_date: string;
+  days: string[];
+  start_time: string;
+  end_time: string;
+  ['teacher.first_name']: string;
+  ['teacher.last_name']: string;
+  subject_id: string;
+  subject_name: string;
+};
+
+export interface ClassPromise extends TypePromise<Class> {}
