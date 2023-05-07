e9e0f118c606966698034c214b146c726438b6c6
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-05-06 20:46:54 -0600

    [REDUX] Update components to get updated reducer values

--- src/routes/classes/Classes.tsx
+++ src/routes/classes/Classes.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { RootState } from 'store/store';
+import { useSelector } from 'react-redux';
 import SectionHeader from 'components/SectionHeader/SectionHeader';
 import CardSkeleton from 'components/CardSkeleton/CardSkeleton';
+import JoinGroupForm from 'components/JoinGroupForm/JoinGroupForm';
+import CreateGroupForm from 'components/CreateGroupForm/CreateGroupForm';
+
+import { Subject } from 'types/Subject/Subject';
+import { getSubjects } from 'utils/db/db.utils';
 
 import styles from './Classes.module.css';
 
 export default function Classes() {
+  const user = useSelector((state: RootState) => state.user.currentUser);
+  const [classes, setClasses] = useState<Subject[]>([]);
+
+  useEffect(() => {
+    const getSubjectsList = async () => {
+      const data = await getSubjects(user?.authToken as string);
+
+      if (data.status === 'success') {
+        setClasses(data.data as Subject[]);
+      } else {
+        console.log(data.data);
+      }
+    };
+
+    getSubjectsList();
+  }, []);
+
   return (
     <>
-      <SectionHeader title="Mis Clases" />
+      <SectionHeader title="Mis Clases">
+        {user?.role === 'teacher' ? (
+          <CreateGroupForm classes={classes} />
+        ) : (
+          <JoinGroupForm />
+        )}
+      </SectionHeader>
       <div className={styles['card-container']}>
         <CardSkeleton items={4} />
       </div>
