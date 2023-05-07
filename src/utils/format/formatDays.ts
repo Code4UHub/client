bb33260d2c7faf7699a1832b292e4376753fcd9d
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-05-07 15:33:13 -0600

    [JOIN GROUP] Fixed formatDays function

--- src/utils/format/formatDays.ts
+++ src/utils/format/formatDays.ts
@@ -1,9 +1,12 @@
+/* eslint-disable prefer-template */
 import { FORMAT_DAYS, Days } from 'types/Days/Days';
 
 export function formatDays(days: string[]) {
-  return days.reduce((acc, day, index) =>
-    index === days.length - 1
-      ? `${FORMAT_DAYS[day as Days]}`
-      : `${FORMAT_DAYS[day as Days]}, `
+  return days.reduce(
+    (acc, day, index) =>
+      index === days.length - 1
+        ? acc + `${FORMAT_DAYS[day as Days]}`
+        : acc + `${FORMAT_DAYS[day as Days]}, `,
+    ''
   );
 }
