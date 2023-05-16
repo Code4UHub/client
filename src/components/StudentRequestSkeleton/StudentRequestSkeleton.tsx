6905d002252bf042e51950535e2b65feb11e868c
Carlos Vega <A01731416@itesm.mx>
2023-05-16 13:07:38 -0600

    [JOIN LIST] Created skeleton for requests

---
+++ src/components/StudentRequestSkeleton/StudentRequestSkeleton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import style from './StudentRequestSkeleton.module.css';
+
+const HEADERS = 6;
+
+type Props = {
+  skeletons: number;
+};
+
+function RequestRow() {
+  return (
+    <tbody>
+      <tr className={style.request}>
+        {Array.from({ length: HEADERS }, (_val, index) => (
+          <td key={index}>
+            <div className={style.skeleton} />
+          </td>
+        ))}
+      </tr>
+    </tbody>
+  );
+}
+
+export default function StudentRequestSkeleton({ skeletons }: Props) {
+  return (
+    <table>
+      {Array.from({ length: skeletons }, (_, index) => (
+        <RequestRow key={index} />
+      ))}
+    </table>
+  );
+}
