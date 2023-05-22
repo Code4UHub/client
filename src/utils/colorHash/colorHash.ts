ea06f90ce931e565a9b30b28db33c2e0c9e03d93
Carlos Vega <A01731416@itesm.mx>
2023-05-22 01:04:13 -0600

    [MUTANT][COLORHASH] Changed rgb order to grb

--- src/utils/colorHash/colorHash.ts
+++ src/utils/colorHash/colorHash.ts
@@ -7,7 +7,7 @@ export function colorHash(s: string) {
   // Generate a hash code for the input string
   let hash = 0;
   for (let i = 0; i < s.length; i++) {
-    hash = s.charCodeAt(i) - ((hash << 5) - hash);
+    hash = s.charCodeAt(i) + ((hash << 5) - hash);
   }
 
   // Convert the hash code to a hex string
@@ -19,9 +19,9 @@ export function colorHash(s: string) {
   const b = parseInt(hexCode.substring(4, 6), 16);
   const brightness = 0.7; // Adjust as desired
   const pastelColor =
-    ((r - (255 - r) * brightness) << 16) |
-    ((g - (255 - g) * brightness) << 8) |
-    ((b - (255 - b) * brightness) << 0);
+    ((b + (255 - b) * brightness) << 16) |
+    ((r + (255 - r) * brightness) << 8) |
+    ((g + (255 - g) * brightness) << 0);
 
   // Convert the pastel color to a hex string and return it
   return '#' + pastelColor.toString(16).padStart(6, '0');
