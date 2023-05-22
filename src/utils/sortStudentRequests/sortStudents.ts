96f37a8337b805dbfba1f2c84916032346f4f348
Carlos Vega <A01731416@itesm.mx>
2023-05-22 02:04:32 -0600

    [MUTANT][REQUEST] Forced function to return values always UP

--- src/utils/sortStudentRequests/sortStudents.ts
+++ src/utils/sortStudentRequests/sortStudents.ts
@@ -6,29 +6,29 @@ export function sortStudents(
   value: 'Up' | 'Down'
 ) {
   // Translate element from spanish (UI) to english (values in backend)
-  const assignedElement: keyof StudentRequest = 'class_id';
-  // switch (element) {
-  //   case 'Grupo':
-  //     assignedElement = 'class_id';
-  //     break;
-  //   case 'Nombre':
-  //     assignedElement = 'first_name';
-  //     break;
-  //   case 'Apellidos':
-  //     assignedElement = 'last_name';
-  //     break;
-  //   case 'Matrícula':
-  //     assignedElement = 'student_id';
-  //     break;
-  //   case 'Fecha':
-  //     assignedElement = 'request_date';
-  //     break;
-  //   default:
-  //     assignedElement = 'class_id';
-  //     break;
-  // }
+  let assignedElement: keyof StudentRequest = 'class_id';
+  switch (element) {
+    case 'Grupo':
+      assignedElement = 'class_id';
+      break;
+    case 'Nombre':
+      assignedElement = 'first_name';
+      break;
+    case 'Apellidos':
+      assignedElement = 'last_name';
+      break;
+    case 'Matrícula':
+      assignedElement = 'student_id';
+      break;
+    case 'Fecha':
+      assignedElement = 'request_date';
+      break;
+    default:
+      assignedElement = 'class_id';
+      break;
+  }
   if (assignedElement === 'class_id') {
-    return value === 'Up'
+    return value === 'Up' || value === 'Down'
       ? data.sort((a, b) =>
           `${a.subject_id}.${a.class_id}`.localeCompare(
             `${b.subject_id}.${b.class_id}`
@@ -40,7 +40,7 @@ export function sortStudents(
           )
         );
   }
-  return value === 'Up'
+  return value === 'Up' || value === 'Down'
     ? data.sort((a, b) => a[assignedElement].localeCompare(b[assignedElement]))
     : data.sort((a, b) => b[assignedElement].localeCompare(a[assignedElement]));
 }
