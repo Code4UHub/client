ca11679fa72aca170a2c68e6303a8ba8c9da6042
Carlos Ruiz <caruiz@mxn2000124.lan>
2023-05-23 21:26:53 -0500

    [CLASS PROGRESS CARD] Update component logic to handle teacher and student user role

--- src/routes/class/home/Home.tsx
+++ src/routes/class/home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 
-import ClassProgress from 'components/ClassProgress/ClassProgress';
+import ClassProgressCard from 'components/ClassProgressCard/ClassProgressCard';
 import ContinueActivityCard from 'components/ContinueActivityCard/ContinueActivityCard';
 import UpcomingHomeworkCard from 'components/UpcomingHomeworkCard/UpcomingHomeworkCard';
 import HomeLeaderboardPositionCard from 'components/HomeLeaderboardPositionCard/HomeLeaderboardPositionCard';
@@ -12,7 +12,7 @@ import styles from './Home.module.css';
 function StudentHome() {
   return (
     <section className={styles['home-container']}>
-      <ClassProgress className={styles['home-container-item']} />
+      <ClassProgressCard className={styles['home-container-item']} />
       <ContinueActivityCard className={styles['home-container-item']} />
       <UpcomingHomeworkCard className={styles['home-container-item']} />
       <HomeLeaderboardPositionCard className={styles['home-container-item']} />
