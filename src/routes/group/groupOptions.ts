f74b363592be2807da30422951c4a1aa17030e93
Carlos Vega <A01731416@itesm.mx>
2023-05-25 21:19:03 -0600

    [GRAPH] Updated GroupOptions for Front

--- src/routes/group/groupOptions.ts
+++ src/routes/group/groupOptions.ts
@@ -1,11 +1,4 @@
-type GroupOptions = {
-  title: string;
-  description: string;
-  information: string;
-  category: 'Leaderboard' | 'Módulo' | 'Tema';
-  evaluate: 'Puntaje' | 'Promedio' | 'Progreso';
-  url: string;
-};
+import { GroupOptions } from 'types/GroupOptions/GroupOptions';
 
 export const groupOptions: GroupOptions[] = [
   {
@@ -15,7 +8,7 @@ export const groupOptions: GroupOptions[] = [
       'Sumatoria del puntaje, considerando los aciertos de tareas y actividades disponibles para los estudiantes, y recompensando acorde al nivel de dificultad de los ejercicios',
     category: 'Leaderboard',
     evaluate: 'Puntaje',
-    url: '../group-leaderboard',
+    url: '../graph/0',
   },
   {
     title: 'Promedio por módulo',
@@ -24,7 +17,7 @@ export const groupOptions: GroupOptions[] = [
       'Promedio de las calificaciones de todos los estudiantes, dividido en módulos.',
     category: 'Módulo',
     evaluate: 'Promedio',
-    url: '../score-module',
+    url: '../graph/1',
   },
   {
     title: 'Promedio por tema',
@@ -33,7 +26,7 @@ export const groupOptions: GroupOptions[] = [
       'Promedio de las calificaciones de todos los estudiantes, dividido en temas.',
     category: 'Tema',
     evaluate: 'Promedio',
-    url: '../score-topic',
+    url: '../graph/2',
   },
   {
     title: 'Progreso por módulo',
@@ -42,7 +35,7 @@ export const groupOptions: GroupOptions[] = [
       'Porcentaje de estudiantes que han obtenido una calificación global de 70 o superior, por cada módulo.',
     category: 'Módulo',
     evaluate: 'Progreso',
-    url: '../progress-module',
+    url: '../graph/3',
   },
   {
     title: 'Progreso por tema',
@@ -51,6 +44,6 @@ export const groupOptions: GroupOptions[] = [
       'Porcentaje de estudiantes que han obtenido una calificación de 70 o superior, por cada tema.',
     category: 'Tema',
     evaluate: 'Progreso',
-    url: '../progress-topic',
+    url: '../graph/4',
   },
 ];
