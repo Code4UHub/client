d6addab322add09713c18e58a56f3233b0dadc3c
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-05-31 14:45:24 -0600

    [TYPES] Create HomeworkRequest type

--- src/types/Homework/Homework.ts
+++ src/types/Homework/Homework.ts
@@ -1,10 +1,21 @@
 import { TypePromise } from 'types/TypePromise/TypePromise';
+import { Question } from 'routes/homework/dummyData';
 
 export type Homework = {
   homework_id: string;
   homework_name: string;
   is_finished: boolean;
   date: Date;
+};
+
+export type HomeworkRequest = {
+  class_id: string | { id: string; value: string };
+  difficulty_id: 1 | 2 | 3;
+  title: string;
+  open_questions: number | undefined;
+  closed_questions: number | undefined;
+  deadline: string;
+  questions_ids: Question[];
 };
 
 export interface HomeworkPromise extends TypePromise<Homework[][]> {}
