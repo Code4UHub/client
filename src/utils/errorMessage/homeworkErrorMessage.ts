81563682911eb6a0b1f79dcd02d053abc58e8605
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-05-31 14:42:43 -0600

    [CREATE HOMEWORK] Add input error messages

---
+++ src/utils/errorMessage/homeworkErrorMessage.ts
@@ -0,0 +1,9 @@
+export enum ClassError {
+  noOption = 'Selecciona una opción de la lista',
+}
+
+export enum NumberError {
+  empty = 'Campo Vacío',
+  noNegatives = 'Sólo números positivos',
+  noDecimal = 'Sólo enteros',
+}
