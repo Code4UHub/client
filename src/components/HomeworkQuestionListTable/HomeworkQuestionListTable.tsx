6152e09e6c0dc041e284e758e396b63ca1539d5e
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-06-05 00:22:02 -0600

    [HOMEWORK QUESTION LIST TABLE] Add Pagination component to table

--- src/components/HomeworkQuestionListTable/HomeworkQuestionListTable.tsx
+++ src/components/HomeworkQuestionListTable/HomeworkQuestionListTable.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   HomeworkQuestionList,
   HomeworkQuestion,
 } from 'types/Questions/Question';
 import QuestionTableRow from 'components/QuestionTableRow/QuestionTableRow';
+import Pagination from 'components/Pagination/Pagination';
 
 import styles from './HomeworkQuestionListTable.module.css';
+
+const ROWS_PER_PAGE = 10;
 
 type Props = {
   questionList: HomeworkQuestionList;
@@ -20,26 +23,48 @@ export default function HomeworkQuestionListTable({
   onChecked,
   onUnchecked,
 }: Props) {
+  const [activePage, setActivePage] = useState(1);
+
+  const numberOfPages =
+    questionList.length % ROWS_PER_PAGE !== 0
+      ? Math.floor(questionList.length / ROWS_PER_PAGE) + 1
+      : questionList.length / ROWS_PER_PAGE;
+
+  const paginatedRows = questionList.filter(
+    (_question, index) =>
+      index >= (activePage - 1) * ROWS_PER_PAGE &&
+      index < (activePage - 1) * ROWS_PER_PAGE + ROWS_PER_PAGE
+  );
+
   return (
-    <table className={styles.table}>
-      <tbody>
-        <tr>
-          <th>Estado</th>
-          <th>Pregunta</th>
-          <th>Módulo</th>
-          <th>Tipo</th>
-          <th>Dificultad</th>
-        </tr>
-        {questionList.map((question) => (
-          <QuestionTableRow
-            questionObj={question}
-            key={question.question_h_id}
-            onChecked={onChecked}
-            onUnchecked={onUnchecked}
-            checked={selectedQuestions.includes(question)}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <div className={styles['table-container']}>
+        <table className={styles.table}>
+          <tbody>
+            <tr>
+              <th>Estado</th>
+              <th>Pregunta</th>
+              <th>Módulo</th>
+              <th>Tipo</th>
+              <th>Dificultad</th>
+            </tr>
+            {paginatedRows.map((question) => (
+              <QuestionTableRow
+                questionObj={question}
+                key={question.question_h_id}
+                onChecked={onChecked}
+                onUnchecked={onUnchecked}
+                checked={selectedQuestions.includes(question)}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Pagination
+        pages={numberOfPages}
+        selectedPage={activePage}
+        onChange={(newPage) => setActivePage(newPage)}
+      />
+    </div>
   );
 }
