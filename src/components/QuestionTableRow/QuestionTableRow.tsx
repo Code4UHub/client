5047fb475165fd64afdf031b2f71fc8e84dc6066
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-06-06 07:35:07 -0600

    [QUESTION TABLE ROW] Update component to open question overview on clicking row

--- src/components/QuestionTableRow/QuestionTableRow.tsx
+++ src/components/QuestionTableRow/QuestionTableRow.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import {
   ClosedHomeworkQuestion,
   OpenHomeworkQuestion,
 } from 'types/Questions/Question';
+import Modal from 'components/Modal/Modal';
 
 import styles from './QuestionTableRow.module.css';
 
@@ -22,12 +23,16 @@ export default function QuestionTableRow({
   onChecked,
   onUnchecked,
 }: QuestionTableRowProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModalRef = useRef<HTMLButtonElement>(null);
+
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  const { question_h_id, question, type, difficulty_id } = questionObj;
+  const { question_h_id, question, type, title } = questionObj;
 
-  const { title, topic } = question;
-
-  const handleCheckBox = () => {
+  const handleCheckBox = (
+    e: React.MouseEvent<HTMLInputElement> | React.KeyboardEvent
+  ) => {
+    e.stopPropagation();
     if (checked) {
       onUnchecked(questionObj.question_h_id);
     } else {
@@ -36,21 +41,59 @@ export default function QuestionTableRow({
   };
 
   return (
-    <tr className={`${styles['table-row']} ${checked ? styles.selected : ''}`}>
-      <td>
-        <input
-          type="checkbox"
-          checked={checked}
-          onClick={handleCheckBox}
-          onChange={() => {}}
-        />
-      </td>
-      <td
-        title={`${question_h_id}. ${title}`}
-      >{`${question_h_id}. ${title}`}</td>
-      <td title={topic}>{topic}</td>
-      <td>{type}</td>
-      <td>{difficulty_id}</td>
-    </tr>
+    <>
+      <Modal
+        title=""
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        lastFocusableElement={closeModalRef}
+        isOpen={isModalOpen}
+      >
+        <div className={styles['preview-container']}>
+          <span
+            className={styles['preview-title']}
+          >{`${question_h_id} - ${question.title}`}</span>
+          <span className={styles['preview-description-title']}>
+            Descripción
+          </span>
+          <code className={styles['preview-description']}>
+            {question.description.replace(/\r/g, '\n')}
+          </code>
+          <button
+            type="button"
+            onClick={() => setIsModalOpen(false)}
+            ref={closeModalRef}
+            className={styles['preview-close']}
+          >
+            Cerrar
+          </button>
+        </div>
+      </Modal>
+      <tr
+        className={`${styles['table-row']} ${checked ? styles.selected : ''}`}
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') setIsModalOpen(true);
+        }}
+        onClick={() => setIsModalOpen(true)}
+      >
+        <td>
+          <input
+            type="checkbox"
+            checked={checked}
+            onClick={handleCheckBox}
+            onChange={() => {}}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') handleCheckBox(e);
+            }}
+          />
+        </td>
+        <td
+          title={`${question_h_id}. ${question.title}`}
+        >{`${question_h_id}. ${question.title}`}</td>
+        <td title={title}>{title}</td>
+        <td>{type}</td>
+      </tr>
+    </>
   );
 }
