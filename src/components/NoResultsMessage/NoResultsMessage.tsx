62b274ea3468325ff9cb8b196935256443abdfff
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-06-09 16:13:15 -0600

    [NO RESULTS MESSAGE] Create component

---
+++ src/components/NoResultsMessage/NoResultsMessage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './NoResults.module.css';
+
+type Props = {
+  className?: string;
+  message: string;
+};
+
+export default function NoResultsMessage({ className, message }: Props) {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <span> {message} 🎉</span>
+    </div>
+  );
+}
+NoResultsMessage.defaultProps = {
+  className: '',
+};
