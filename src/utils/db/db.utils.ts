d53b4ce9b2eda18ae659e759010c6595922a66cc
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-06-09 16:24:08 -0600

    [API] Add server time, incoming challenge, homeworks endpoints

--- src/utils/db/db.utils.ts
+++ src/utils/db/db.utils.ts
@@ -17,19 +17,23 @@ import {
   UpdateModule,
 } from 'types/Module/Module';
 import {
+  HomeworkPromise,
   HomeworkRequest,
   HomeworkResponsePromise,
 } from 'types/Homework/Homework';
+import { Challenge } from 'types/Challenge/Challenge';
+import { HomeworkQuestionListPromise } from 'types/Questions/Question';
+import { ServerTimePromise } from 'types/ServerTime/ServerTime';
+
+import { formatDateString } from 'utils/format/formatDate';
 
 // TODO: Delete when backend has this information
-import { leaderboardData } from 'routes/groupGraphController/leaderboardDummyData';
+import { leaderboardData } from 'routes/class/groupGraphController/leaderboardDummyData';
 import { GroupGraphPromise } from 'types/GroupGraph/GroupGraphType';
-import { HomeworkQuestionListPromise } from 'types/Questions/Question';
 
 // http://ec2-3-140-188-143.us-east-2.compute.amazonaws.com:65534/v1
 // http://10.147.20.218:65534/v1
-const BASE_URL =
-  'http://ec2-3-140-188-143.us-east-2.compute.amazonaws.com:65534/v1';
+const BASE_URL = 'http://10.147.20.218:65534/v1';
 
 const ENDPOINTS = {
   STUDENT_REGISTER: `${BASE_URL}/student/register`,
@@ -55,6 +59,13 @@ const ENDPOINTS = {
     `${BASE_URL}/homework/question/subject/${subject_id}/difficulty/${difficulty}`,
   SUBJECT_MODULES: (id: string) => `${BASE_URL}/subject/${id}/modules`,
   HOMEWORK: `${BASE_URL}/homework`,
+  SERVER_TIME: `${BASE_URL}/configuration/time`,
+  CLASS_HOMEWORK: (class_id: string, startDate: Date, endDate?: Date) =>
+    `${BASE_URL}/class/${class_id}/homework?startDate=${formatDateString(
+      startDate
+    )}${endDate ? `&endDate=${formatDateString(endDate)}` : ''}`,
+  INCOMING_CHALLENGE: (id_class: string, id_student: string) =>
+    `${BASE_URL}/challenge/class/${id_class}/student/${id_student}/incoming_challenge`,
 };
 
 export const createStudent = async (user: {
@@ -461,6 +472,56 @@ export const createHomework = async (
   };
 
   const request = await fetch(ENDPOINTS.HOMEWORK, options);
+
+  return request.json();
+};
+
+// GET SERVER TIME
+export const getServerTime = async (
+  auth_token: string
+): Promise<ServerTimePromise> => {
+  const request = await fetch(ENDPOINTS.SERVER_TIME, {
+    headers: {
+      Authorization: `Bearer ${auth_token}`,
+    },
+  });
+
+  return request.json();
+};
+
+// GET CLASS HOMEWORK
+export const getClassHomeworkList = async (
+  auth_token: string,
+  class_id: string,
+  startDate: Date,
+  endDate?: Date
+): Promise<HomeworkPromise> => {
+  const request = await fetch(
+    ENDPOINTS.CLASS_HOMEWORK(class_id, startDate, endDate),
+    {
+      headers: {
+        Authorization: `Bearer ${auth_token}`,
+      },
+    }
+  );
+
+  return request.json();
+};
+
+// GET INCOMIG CHALLENGE
+export const getIncomingChallenge = async (
+  auth_token: string,
+  class_id: string,
+  student_id: string
+): Promise<TypePromise<Challenge | {}>> => {
+  const request = await fetch(
+    ENDPOINTS.INCOMING_CHALLENGE(class_id, student_id),
+    {
+      headers: {
+        Authorization: `Bearer ${auth_token}`,
+      },
+    }
+  );
 
   return request.json();
 };
