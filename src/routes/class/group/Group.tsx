d3efd7e909b96bea7614eef2d55b11aecf6bac83
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-06-09 16:16:48 -0600

    [CLASS HOMEWORK] Move files and add Homework section in class page

---
+++ src/routes/class/group/Group.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from 'components/Card/Card';
+
+import { groupOptions } from './groupOptions';
+
+import style from './Group.module.css';
+
+export default function Group() {
+  return (
+    <div className={style.container}>
+      {groupOptions.map((option, i) => (
+        <Link
+          to={`graph/${i}`}
+          state={{ id_graph: i }}
+          key={option.title}
+        >
+          <Card className={style.card}>
+            <h3 className={style.title}>{option.title}</h3>
+            <p className={style.description}>{option.description}</p>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  );
+}
