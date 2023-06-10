753c622539826bc99b08726dc643aebe6eeb0673
Carlos24Rz <carlos_ruiz1002@hotmail.com>
2023-06-10 01:38:29 -0600

    [CHALLENGE TYPE] Update Type values

--- src/types/Challenge/Challenge.ts
+++ src/types/Challenge/Challenge.ts
@@ -1,12 +1,12 @@
 import { TypePromise } from 'types/TypePromise/TypePromise';
 
 export type Challenge = {
-  module_id: number;
-  title: string;
-  is_active: true;
-  score: number;
   challenge_id: number;
+  module_title: string;
   challenge_title: string;
+  student_id: string;
+  status: 'start' | 'continue';
+  difficulty: string;
 };
 
 export interface ChallengePromise extends TypePromise<Challenge> {}
